e828819677fd54f930cef5a9aa75cc279a398943
mostafa_stn <mostafa_stn@yahoo.com>
2025-04-20 12:26:38 +0330

    Add project files.

---
+++ RabbitConsumerSample/TextConsumer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitConsumerSample
+{
+    public class TextConsumer : BackgroundService
+    {
+        private readonly ILogger<TextConsumer> _logger;
+
+        public TextConsumer(ILogger<TextConsumer> logger)
+        {
+            _logger = logger;            
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("👂 TextConsumer is starting...");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "sample-queue", durable: false, exclusive: false, autoDelete: false);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                _logger.LogInformation($"📩 Received message: {message}");
+                await Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync(queue: "sample-queue", autoAck: true, consumer: consumer);
+            _logger.LogInformation("✅ Listening for messages ...");
+
+            // فقط منتظر بمون تا سرویس متوقف بشه
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+
+            _logger.LogInformation("🛑 TextConsumer is stopping.");
+        }
+    }
+}
---
+++ RabbitPublisherSample/TextPublisher.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitPublisherSample
+{
+    public class TextPublisher : BackgroundService
+    {
+        private readonly ILogger<TextPublisher> _logger;
+
+        public TextPublisher(ILogger<TextPublisher> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        //{
+        //    _logger.LogTrace("Background Service is starting.");
+
+        //    while (!stoppingToken.IsCancellationRequested)
+        //    {
+        //        _logger.LogTrace("Background Service is working at: {time}", DateTimeOffset.Now);
+        //        await Task.Delay(5000, stoppingToken); // اجرای هر ۵ ثانیه یک بار
+        //    }
+
+        //    _logger.LogTrace("Background Service is stopping.");
+
+
+
+        //    var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        //    using var connection = await factory.CreateConnectionAsync();
+        //    using var channel =await connection.CreateChannelAsync();
+
+        //    // تعریف صف
+        //    await channel.QueueDeclareAsync(queue: "sample-queue", durable: false, exclusive: false, autoDelete: false);
+
+        //    // پیام ارسالی
+        //    string message = "پیامی از Publisher با .NET 9!";
+        //    var body = Encoding.UTF8.GetBytes(message);
+
+        //    // ارسال
+        //    await channel.BasicPublishAsync(exchange: "", routingKey: "sample-queue", basicProperties: null, body: body, cancellationToken: stoppingToken);
+        //    Console.WriteLine($"✅ پیام ارسال شد: {message}");
+
+
+        //}
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("📡 Background Service is starting.");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "sample-queue", durable: false, exclusive: false, autoDelete: false);
+
+            int count = 1;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                string message = $"📨 Message number {count++} from Publisher in {DateTime.Now}";
+                var body = Encoding.UTF8.GetBytes(message);
+                                
+                await channel.BasicPublishAsync(
+                    exchange: "",
+                    routingKey: "sample-queue", // فقط نام صف                    
+                    body: Encoding.UTF8.GetBytes(message),
+                    cancellationToken: stoppingToken
+                    );
+
+                _logger.LogInformation("✅ Message sent: {msg}", message);
+
+                await Task.Delay(5000, stoppingToken); // هر ۵ ثانیه یک بار
+            }
+
+            _logger.LogInformation("🛑 Background Service is stopping.");
+        }
+    }
+}
